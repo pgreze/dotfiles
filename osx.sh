c32af7cb36f8461043f380f3b53b77d2a91bf2e8
pgreze <pierrick.greze@gmail.com>
2017-07-26 23:12:46 +0900

    Improve OSX config and always run it if Darwin is detected

--- osx.sh
+++ osx.sh
@@ -1,8 +1,6 @@
 ##
-## Common OSX configuration part
+## Common OSX configuration
 ##
-
-## VARS
 
 # Python
 export PIP_DOWNLOAD_CACHE='/var/tmp/pip-cache';
@@ -11,18 +9,17 @@ export PIP_DOWNLOAD_CACHE='/var/tmp/pip-cache';
 export HOMEBREW_CASK_OPTS='--appdir=/Applications';
 
 # Promote gnu utils from homebrew
-if [ $(brew list | grep coreutils | wc -l) != 0 ];then
-    PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
-
+if [ -d /usr/local/opt/coreutils ];then
+    PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
     # Fix ls alias when gnu utils are used
     alias ls='ls --color'
 fi
 
-# fix encoding annoyances with less. See configuration with "locale" command.
+# Fix encoding annoyances with less. See configuration with "locale" command.
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Colored ls with yellow for directories
-# Not working with homebrew ls :/ 
+# Not working with homebrew ls :/
 export CLICOLOR=1
 export LSCOLORS=dxfxcxdxbxegedabagacad
