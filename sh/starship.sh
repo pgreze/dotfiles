b800fb0f14eb3ada75e3ea1bb0dd5e92962d24bc
pgreze <pierrick.greze@gmail.com>
2020-02-22 13:44:26 +0900

    Replace custom theme by starship

---
+++ sh/starship.sh
@@ -0,0 +1,48 @@
+###
+### Starship configuration.
+### See https://starship.rs/config/ for customization.
+###
+
+function init_starship {
+  # Install if not loaded + missing in path
+  if [ -z "$STARSHIP_SHELL" ] && [ ! command_exists starship ]; then
+    echo ">> Install starship"
+    case "$OSTYPE" in
+      darwin*) brew install starship ;;
+      *)       curl -fsSL https://starship.rs/install.sh | bash ;;
+    esac
+  fi
+
+  generate_starship_config
+}
+
+function generate_starship_config {
+  local config_file="$HOME/.config/starship.toml"
+  mkdir -p "$HOME/.config"
+  case "$OSTYPE" in
+    darwin*) cat > $config_file <<EOF
+[character]
+symbol = "🍏 ➜ "
+error_symbol = "🍎 ➜ "
+use_symbol_for_status = true
+EOF
+    ;;
+    linux*) cat > $config_file <<EOF
+[character]
+symbol = "🐧 ➜ "
+error_symbol = "🧨 ➜ "
+use_symbol_for_status = true
+EOF
+    ;;
+  esac
+}
+
+# https://starship.rs/guide/#getting-started
+case $SHELL in
+  */zsh)  init_starship && eval "$(starship init zsh)" ;;
+  */bash) init_starship && eval "$(starship init bash)" ;;
+  *)      echo "Unsupported shell, cannot load starship."
+esac
+
+unset -f init_starship
+unset -f generate_starship_config
