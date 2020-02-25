dffe3015f281a3d780153da6d54ef74abd648834
pgreze <pierrick.greze@gmail.com>
2020-02-25 02:43:52 +0000

    Add missing local environment usage

--- sh/env.sh
+++ sh/env.sh
@@ -6,3 +6,9 @@
 export PATH="$DOTFILES/bin:$PATH"
 
 command -v code > /dev/null && export EDITOR=code || export EDITOR=vim
+
+if [ -e $DOTFILES/local.sh ]; then
+  source "$DOTFILES/local.sh"
+else
+  echo "Specify $DOTFILES/local.sh for your local specific config"
+fi
