69f6971135350907af5f3498f45d466b4917bf69
pgreze <pierrick.greze@gmail.com>
2020-05-02 12:18:58 +0900

    Add java.sh:jdk command

---
+++ sh/java.sh
@@ -0,0 +1,16 @@
+###
+### Java configuration
+###
+
+# https://github.com/AdoptOpenJDK/homebrew-openjdk
+jdk() {
+    local VERSION=$1
+    if [ -z "$VERSION" ]; then
+        printf "Usage: jdk (8,9,11,13...)\n\n"
+        /usr/libexec/java_home -V
+    else
+        [ "$VERSION" = "8" ] && VERSION="1.8"
+        export JAVA_HOME=$(/usr/libexec/java_home -v"$VERSION");
+        java -version
+    fi
+}
