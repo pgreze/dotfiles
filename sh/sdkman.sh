6335df3e4de87c625ca7464895d064239da935fb
pgreze <pierrick.greze@gmail.com>
2020-05-04 12:53:22 +0000

    Add sdkman.sh

---
+++ sh/sdkman.sh
@@ -0,0 +1,11 @@
+##
+## https://sdkman.io/ config
+##
+## This is a tool for managing parallel versions of multiple Software Development Kits on most Unix based systems.
+## It provides a convenient Command Line Interface (CLI) and API for installing, switching, removing and listing Candidates.
+##
+## Install with: curl -s "https://get.sdkman.io" | bash
+##
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
