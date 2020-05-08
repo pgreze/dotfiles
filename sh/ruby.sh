f920410ff61728a3a01cac0e15d98a94ad5421c0
pgreze <pierrick.greze@gmail.com>
2020-05-08 19:00:11 +0900

    Ignore rbenv command output

--- sh/ruby.sh
+++ sh/ruby.sh
@@ -5,6 +5,6 @@
 # Load rbenv if available
 # https://github.com/rbenv/rbenv
 [ -e $HOME/.rbenv ] && export PATH="$HOME/.rbenv/bin:$PATH"
-if command -v rbenv; then
+if command -v rbenv > /dev/null; then
   eval "$(rbenv init -)"
 fi
