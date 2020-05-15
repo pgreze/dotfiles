c7cf281d281004a1e133ab9f4de7884936dcd23b
pgreze <pierrick.greze@gmail.com>
2020-05-15 16:08:18 +0900

    Add pr_merged util

--- sh/git.sh
+++ sh/git.sh
@@ -23,3 +23,19 @@ function psed {
     local pathspecs="${@:3}"
     git grep --null --full-name --name-only --perl-regexp -e "$search_reg" $pathspecs | xargs -0 perl -i -p -e "s:$search_reg:$replace_reg:g"
 }
+
+# Common workflow after a PR was merged
+function pr_merged {
+    local base_branch="${1:-master}"
+    local current_branch="$(git rev-parse --abbrev-ref HEAD)"
+    if [ $(echo "$current_branch" | wc -l) != 1 ]; then
+        echo "More than 1 current branch found: $current_branch" 1>&2
+        return 1
+    elif [ "$current_branch" = "$base_branch" ]; then
+        echo "Already on the base branch ($base_branch)" 1>&2
+        return 1
+    fi
+    git checkout "$base_branch"
+    git pull origin "$base_branch"
+    git branch -D "$current_branch"
+}
