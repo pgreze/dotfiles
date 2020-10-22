2a4137ffc29b61a373ded55fcc49f25a696ce709
pgreze <pierrick.greze@gmail.com>
2020-10-22 13:54:39 +0900

    Improve bash detection

--- sh/bash.sh
+++ sh/bash.sh
@@ -2,7 +2,9 @@
 ### Bash configuration
 ###
 
-if [ $(which -a bash | grep /usr/local/bin/bash | wc -l) -ne 0 ] && \
+# See https://itnext.io/upgrading-bash-on-macos-7138bd1066ba
+if file /usr/local/bin/bash > /dev/null && \
+    [ $(which -a bash | grep /usr/local/bin/bash | wc -l) -ne 0 ] && \
     [ $(grep /usr/local/bin/bash /etc/shells | wc -l) -eq 0 ]; then
     echo "Consider adding the non default bash as available shell with:"
     echo "sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'"
