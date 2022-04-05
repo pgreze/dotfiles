71c80de4f1c9f50a0b37c2fb8e908e712031cd32
pgreze <pierrick.greze@gmail.com>
2022-04-05 15:43:57 +0900

    Improves the jdk command

--- sh/java.sh
+++ sh/java.sh
@@ -8,9 +8,10 @@ jdk() {
     if [ -z "$VERSION" ]; then
         printf "Usage: jdk (8,9,11,13...)\n\n"
         /usr/libexec/java_home -V
+        echo "\nVersion: "
     else
         [ "$VERSION" = "8" ] && VERSION="1.8"
         export JAVA_HOME=$(/usr/libexec/java_home -v"$VERSION");
-        java -version
     fi
+    java -version
 }
