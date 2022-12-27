67b41b7a40f53aefbc232879ecb7b70d5168768d
pgreze <pierrick.greze@gmail.com>
2022-12-27 15:07:50 +0900

    Add yamllint config

---
+++ sh/yamllint.sh
@@ -0,0 +1,10 @@
+###
+### https://github.com/adrienverge/yamllint
+###
+
+if command -v yamllint > /dev/null; then
+    _yamllint="$(command -v yamllint)"
+    alias yamllint="$_yamllint -c $HOME/.yamllint.yml"
+elif [[ "$(uname)" == 'Darwin' ]]; then
+    echo "TODO: brew install yamllint"
+fi
